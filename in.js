0bd510dd8741f1f7228fc9dcbd0cf0dbd3a3ba2e
KiranASC6 <kirannarine131@gmail.com>
2019-08-06 15:50:14 -0400

    Most of home page done

---
+++ in.js
@@ -0,0 +1,17 @@
+function xd(){
+    if(document.getElementById("butt-on-xd").innerHTML==""){
+        document.getElementById("butt-on-xd").innerHTML="there's nothing here lmao xd (get prankt bro gotteem 😂😂😂👌👌👌) (sorry for the prank, our instructor forced us to have a button for these webpages)";
+    }else{
+        document.getElementById("butt-on-xd").innerHTML="";
+    }  
+}
+
+//stuff taken off for presentation
+//
+
+
+
+
+// function delete(){
+//     document.getElementById("butt-on-xd").innerHTML= "";
+// }
